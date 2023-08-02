df7ce59dc8adf2e53c11c80decf4602a16cd2edc
jcaluiz <lacjunior129.la@gmail.com>
2023-08-02 14:11:15 -0300

    Trocando a rota da api por ter terminado a versão gratuita do railway

--- src/hooks/useGetPlanet.js
+++ src/hooks/useGetPlanet.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 function useGetPlanet() {
   const [data, setData] = useState([]);
   const getPlanetAPI = async () => {
-    const endpoint = await fetch('https://starwars-api-production.up.railway.app/get_planets');
+    const endpoint = await fetch('https://starwars-oxvej3n7i-jcaluiz.vercel.app/get_planets');
     const dataFetch = await endpoint.json();
     setData(dataFetch);
   };
--- src/services/requests.js
+++ src/services/requests.js
@@ -1,5 +1,5 @@
 const getPlanetAPI = async () => {
-  const endpoint = await fetch('https://starwars-api-production.up.railway.app/get_planets');
+  const endpoint = await fetch('https://starwars-oxvej3n7i-jcaluiz.vercel.app/get_planets');
   const dataFetch = await endpoint.json();
   console.log(dataFetch);
   return dataFetch;
